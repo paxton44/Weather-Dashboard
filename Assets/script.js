fbf8e395df18c7a8268926084c77b85c30776ca3
Mitchell Flood <mitchflood1@gmail.com>
2021-03-15 12:15:16 -0600

    got lat and lon to log to the console for uv index

--- Assets/script.js
+++ Assets/script.js
@@ -4,6 +4,8 @@ console.log("JR full stack developer seeking employment! Contact me on github @p
 //Api key 
 var ApiKey = "6954046ca35cd79c3298ba9167dbb29f";
 
+
+
 //get api function
 
 function getApi(e) {
@@ -12,16 +14,12 @@ function getApi(e) {
 
   var searchField = $("#searchButton").on("click", getApi)
   var value = $(this).siblings('#search-input').val();
-  // console.log(value);
+  console.log(value);
 
   //Set local storage
   localStorage.setItem(searchField, value);
   // console.log(localStorage);
-  console.log("<------------------------->");
-
-
-
-
+  // console.log("<------------------------->");
 
   // fetch request used imperial units to convert to usa standards 
   var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + value + '&units=imperial&appid=' + ApiKey;
@@ -36,16 +34,21 @@ function getApi(e) {
 
       //display city name, temperature, humidity, wind speed, and the weather image 
       // not displaying weather image and uv index 
+     
       var currentCityName = data.name
       var temp = data.main.temp + " °F"
       var humidity = data.main.humidity + " %"
       var wind = data.wind.speed + " MPH"
-      var weatherImg = 'https://openweathermap.org/img/w/' + data.weather[0].icon + '.png';
+      var weatherImg = 'https://openweathermap.org/img/w/' + data.weather[0].icon + '.png'
+     
+
+      
       console.log(currentCityName);
-      // console.log(temp);
-      // console.log(humidity);
-      // console.log(wind);
+      console.log(temp);
+      console.log(humidity);
+      console.log(wind);
       console.log(weatherImg);
+     
 
       //display data from API in HTML using the .text method
       $("#current-city").text(currentCityName)
@@ -53,41 +56,24 @@ function getApi(e) {
       $("#humidity").text(humidity)
       $("#wind-speed").text(wind)
       $("#current-img").attr('src', weatherImg);
-
-
-
-      var lat = data.coord.lat;
-      var lon = data.coord.lon;
-
-      var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-      };
-
+      
+      //UV index
+      var lon = data.coord.lon
+      var lat = data.coord.lat
+      console.log(lon)
+      console.log(lat)
     })
 }
+
+
 
 //5 day forecast using moment 
 //We need to display weather icon, temp, and humidity. 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//Event Listener 
 searchButton.addEventListener('click', getApi);
-console.log("<________________>");
-console.log(localStorage);
+// console.log("<________________>");
+// console.log(localStorage);
 
 //display user history .text doest work on input forms
+
